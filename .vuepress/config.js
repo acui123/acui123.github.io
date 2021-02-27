e81947159e9494513c4b8aa8a8d7e7e7862b6674
acui123 <642784158@qq.com>
2021-02-27 21:50:06 +0800

    首次提交代码，ssm整合

---
+++ .vuepress/config.js
@@ -0,0 +1,131 @@
+module.exports = {
+  "title": "xcuiBlog",
+  "description": "学习记录",
+  "dest": "public",
+  plugins: [
+    ["vuepress-plugin-boxx"],
+    [
+      "vuepress-plugin-cat"
+    ],
+    [
+      'meting',
+      {
+        //  metingApi: 'http://music.xf1433.com/',
+        meting: {
+          server: 'netease',
+          type: 'playlist',
+          mid: '2539599584',
+        }, // 不配置该项的话不会出现全局播放器
+        aplayer: {
+          lrcType: 3,
+        },
+      },
+    ],
+
+
+
+
+    [
+      "dynamic-title",
+      {
+        showIcon: "/favicon.ico",
+        showText: "(/≧▽≦/)咦！又好了！",
+        hideIcon: "/failure.ico",
+        hideText: "(●—●)喔哟，崩溃啦！",
+        recoverTime: 2000
+      }
+    ]
+  ],
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    subSidebar: "auto",
+    "nav": [{
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [{
+          "text": "vuepress-reco",
+          "link": "/docs/theme-reco/"
+        }]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [{
+          "text": "GitHub",
+          "link": "https://github.com/recoluan",
+          "icon": "reco-github"
+        }]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [{
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "xCui",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
